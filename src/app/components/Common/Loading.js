8829bdd4582d5a658a9025766bb9ec88ec32f1fe
Fulal-kt <muhamedfulail77@gmail.com>
2024-04-28 22:31:42 +0530

    final commit

--- src/app/components/Common/Loading.js
+++ src/app/components/Common/Loading.js
@@ -1,44 +1,109 @@
-
 'use client'
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
-import loading from '../../../../public/loading1.png';
+import Link from 'next/link';
+import loading1 from '../../../../public/loading1.png';
+import loading2 from '../../../../public/loading2.png';
+import loading3 from '../../../../public/loading3.png';
+import loading3_1 from '../../../../public/loading3.1.png';
+import loading4 from '../../../../public/loading4.png';
+import loading4_1 from '../../../../public/loading4.1.png';
+import loading5 from '../../../../public/loading5.png';
+import loading5_1 from '../../../../public/loading5.1.png';
+import AstrixBlk from '../../../../public/AstrixBlk.png';
 
 const Loading = () => {
-  const [isOpen, setIsOpen] = useState(false); // State to control animation
+    const [width, setWidth] = useState(100);
+    const [swidth, setSwidth] = useState(190);
+    const [twidth, setTwidth] = useState(90);
+    const [fwidth, setFwidth] = useState(110);
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setIsOpen(prevOpen => !prevOpen); // Toggle animation state on interval
-    }, 2000); // Adjust interval for desired animation speed (lower = faster)
 
-    return () => clearInterval(intervalId); // Cleanup on unmount
-  }, []);
 
-  const style = {
-    width: isOpen ? '50%' : '90%', // Set the width based on the isOpen state
-    height: '100%', // Maintain original height
-    objectFit: 'cover', // Maintain aspect ratio and cover container
-    transition: 'width 0.7s ease-in-out', // Add CSS transition for smooth animation
-  };
+    useEffect(() => {
+        const interval = setInterval(() => {
+            // for first (fwidth) image
+            setFwidth(prevWidth => (prevWidth === 110 ? 220 : 110));
+            // for second (swidth) image
+            setSwidth(prevWidth => (prevWidth === 190 ? 350 : 190));
+            // for third (twidth) image
+            setTwidth(prevWidth => (prevWidth === 50 ? 100 : 50));
+            // default
+            setWidth(prevWidth => (prevWidth === 100 ? 200 : 100));
+        }, 1000);
 
-  return (
-    <div className='bg-[#FFF4DF] w-full h-screen'>
-      <div></div>
+        return () => clearInterval(interval);
+    }, []);
 
-      <div className='w-full flex justify-center items-center h-full'>
-        <div className='w-[60%] h-[500px] flex flex-col justify-around bg-blue-400'>
-          <div className='w-full bg-green-200 flex flex-start'>
-            <h1>WE</h1>
-            <div className='h-full' style={style}>
-              <Image src={loading} className='w-full' alt="Loading" />
+    const imageStyle = {
+        width: `${width}px`,
+        height: '20%',
+        transition: 'width 2s ease-in-out',
+    };
+
+    const imageStyle1 = {
+        width: `${fwidth}px`,
+        height: '20%',
+        transition: 'width 2s ease-in-out',
+    };
+
+    const imageStyle2 = {
+        width: `${swidth}px`,
+        height: '20%',
+        transition: 'width 2s ease-in-out',
+    };
+    const imageStyle3 = {
+        width: `${twidth}px`,
+        height: '20%',
+        transition: 'width 2s ease-in-out',
+    };
+
+    return (
+      
+        <Link href='/home'> 
+            <div className='w-full h-screen bg-[#FFF4DF] cursor-pointer'>
+                <div>
+                    <div className='flex items-center p-5'>
+                        <Image src={AstrixBlk} />
+                        <h1 className=" font-bold text-4xl">Astrix.</h1>
+                    </div>
+                </div>
+                <div className='w-full flex justify-center items-center'>
+                    <div className=' w-[60%] flex flex-col justify-center items-center '>
+                        <div className='w-[590px]'>
+                            <div className='flex  gap-4 overflow-hidden items-center  h-40' >
+                                <h1 className='text-4xl font-semibold'>WE</h1>
+                                <Image src={loading1} alt="Image" style={imageStyle1} />
+                                <h1 className='text-4xl font-semibold'>ORGANIZE THE</h1>
+                            </div>
+                            <div className='flex gap-4 -mt-20 items-center overflow-hidden h-40' >
+                                <h1 className='text-4xl font-semibold'>CONNECTION</h1>
+                                <Image src={loading2} alt="Image" style={imageStyle2} />
+                            </div>
+                            <div className='flex -mt-20 gap-4 items-center overflow-hidden h-40' >
+                                <Image src={loading3} alt="Image" style={imageStyle} />
+                                <h1 className='text-4xl font-semibold'>BETWEEN</h1>
+                                <Image src={loading3_1} alt="Image" className='h-6 w-10' />
+                                <h1 className='text-4xl font-semibold'>MUSIC</h1>
+                            </div>
+                            <div className='flex -mt-20  gap-4 items-center overflow-hidden h-40' >
+                                <h1 className='text-4xl font-bold'>ARTIST</h1>
+                                <Image src={loading4} alt="Image" style={imageStyle} />
+                                <h1 className='text-4xl font-bold'>CULTURE</h1>
+                                <Image src={loading4_1} alt="Image" style={imageStyle3} />
+                            </div>
+                            <div className='flex -mt-20 gap-4 items-center overflow-hidden h-40' >
+                                <Image src={loading5} alt="Image" style={imageStyle} />
+                                <h1 className='text-4xl font-bold'>ART</h1>
+                                <Image src={loading5_1} alt="Image" className='h-8 w-12' />
+                                <h1 className='text-4xl font-bold'>&&nbsp;COLLECTIONS</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <h1>Something</h1>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+        </Link>
+    );
 };
 
 export default Loading;
--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,12 +1,94 @@
-import React from 'react'
-import Loading from '../components/Common/Loading'
+'use client'
+import { useState } from "react";
+import Image from "next/image";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css'
 
-const page = () => {
+// static data
+
+import { Data, Events, Fonts, svg, settings } from "../Constants/constant";
+
+// components
+
+import Card from "../components/Common/Card";
+import EventSidebar from "../components/Events/EventSidebar";
+import CollectionSidebar from "../components/Collection/CollectionSidebar";
+
+
+export default function Home() {
+
+  const [collectionTrue, setCollectionTrue] = useState(false)
+
   return (
-    <div>
-      <Loading/>
-    </div>
-  )
-}
+    // Header
 
-export default page
+    <div className="h-screen w-full flex flex-col pl-10">
+      <div className="w-full flex items-center">
+        <Image src={svg.Astrix} alt="astrix logo" />
+        <h1 className="text-[#FFCA5F] font-extrabold text-4xl">Astrix.</h1>
+      </div>
+
+      {/* Slider */}
+
+      <div className="w-full h-full flex items-center  relative">
+        <div className="w-[50%] ml-32 z-20 overflow-x-auto">
+          <div className={`w-full ${Fonts.lexend600.className}`}>
+            {collectionTrue ? (
+              <Slider className="w-full carousel-container" {...settings}>
+                {Data.map((item) => (
+                  <Card key={item.id} data={item} isEvent={false} />
+                ))}
+              </Slider>
+            ) : (
+              <Slider {...settings}>
+                {Events.map((item) => (
+                  <Card key={item.id} data={item} isEvent={true} />
+                ))}
+              </Slider>
+            )}
+          </div>
+        </div>
+
+        {/*  Bacground Font  */}
+
+        <div className={`absolute flex flex-col gap-y-16 ${Fonts.lexend800.className} text-7xl text-start left-0  text-white text-opacity-20 lexend-700   z-0`}>
+          <div className="h-32">
+            <h1>ASTR<br />IX</h1>
+          </div>
+          {
+            collectionTrue ? (
+              <div className="h-52">
+                <h1>COLL<br />ECTI<br />BLE</h1>
+              </div>
+            ) : (
+              <div className="h-52">
+                <h1>EVE<br />NTS</h1>
+              </div>
+            )
+          }
+        </div>
+      </div>
+
+      {/* Sidebars */}
+
+      <div className="absolute flex bg-[#15181B] right-0 z-50 w-[45%] h-full">
+        <div className="w-20 overflow-hidden bg-[#B9A0FF] h-full flex justify-center items-center">
+          <h1 className={`marquee flex gap-2 rotate-90 whitespace-nowrap  text-md ${Fonts.kronaOne.className}`}>
+            Event : Oasis Bus tour , JLN Stadium , Delhi  <Image src={svg.star} width={25} /> Collection Live: Meta Lives, live on astrix
+          </h1>
+        </div>
+        {collectionTrue ? <CollectionSidebar /> : <EventSidebar />}
+      </div>
+
+      {/* Navigation buttons */}
+
+      <div className=" pb-10 text-white font-semibold flex  items-center rounded-full ">
+        <div className=" bg-[#282B30] flex justify-between  rounded-full">
+          <button onClick={() => { setCollectionTrue(false) }} className={`${Fonts.lexend600.className} ${!collectionTrue ? "bg-[#484E56]" : ""} rounded-full px-10 py-2`}>Events</button>
+          <button onClick={() => { setCollectionTrue(true) }} className={`${Fonts.lexend600.className} ${collectionTrue ? "bg-[#484E56]" : ""}  rounded-full px-7`}>Collections</button>
+        </div>
+      </div>
+    </div>
+  );
+}
